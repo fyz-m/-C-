c10c91a301a24139ec866f5a71315a5c7599ac0c
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-02 12:38:52 +0400

    Add Literal and binary nodes

---
+++ src/parser/Expr.cpp
@@ -0,0 +1,9 @@
+#include "Expr.hpp"
+
+BinaryExpr::BinaryExpr(ExprNodePtr left, Token& operation, ExprNodePtr right)
+    : m_leftNode{std::move(left)},
+      m_operation{std::move(operation)},
+      m_rightNode{std::move(right)} {}
+
+LiteralExpr::LiteralExpr(LiteralValue& value)
+    : m_Value{std::move(value)} {}
---
+++ src/parser/Expr.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include "../lexer/Token.hpp"
+
+#include <memory>
+#include <variant>
+
+// This file contains the declarations of AST nodes for Expressions
+
+// Forward declarations for syntatic sugar
+struct BinaryExpr;
+struct LiteralExpr;
+
+// Syntatic sugar for node pointers
+using BinaryExprPtr = std::unique_ptr<BinaryExpr>;
+using LiteralExprPtr = std::unique_ptr<LiteralExpr>;
+
+// Using variant + visit instead of dynamic polymorphism (visitor pattern)
+using ExprNodePtr = std::variant<BinaryExprPtr, LiteralExprPtr>;
+
+// Helper method to create an Expression node as a variant
+// Using template so I don't need to make a new function for every expression node
+// E.g createBinaryExpr, createLiteralExpr etc.
+template <typename T>
+concept ExprType = std::same_as<T, BinaryExpr> || std::same_as<T, LiteralExpr>;
+
+template <ExprType T, typename... Args>
+ExprNodePtr createExprNode(Args... args) {
+    return std::make_unique<T>(std::forward<Args>(args)...);
+}
+
+///////////////////////
+// NODE DECLARATIONS //
+///////////////////////
+
+struct BinaryExpr {
+    const ExprNodePtr m_leftNode;
+    const Token m_operation;
+    const ExprNodePtr m_rightNode;
+
+    BinaryExpr(ExprNodePtr left, Token& operation, ExprNodePtr right);
+};
+
+struct LiteralExpr {
+    const LiteralValue m_Value;
+
+    LiteralExpr(LiteralValue& value);
+};
