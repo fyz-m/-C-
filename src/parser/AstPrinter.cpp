e4e90575694835bbd368af357215776db5010f75
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-03 16:13:27 +0400

    Add stmt print

--- src/parser/AstPrinter.cpp
+++ src/parser/AstPrinter.cpp
@@ -1,10 +1,18 @@
 #include "AstPrinter.hpp"
 
-#include "Ast/Expr.hpp"
-#include "sstream"
+#include <print>
+#include <sstream>
 
-std::string Print(const ExprNodePtr& exprNode) {
+void PrintAST(const ExprNodePtr& exprNode) {
+    std::println("{}", std::visit(PrintExpr{}, exprNode));
+}
+
+std::string Printer::printExpr(const ExprNodePtr& exprNode) {
     return std::visit(PrintExpr{}, exprNode);
+}
+
+std::string Printer::printStmt(const StmtNodePtr& stmtNode) {
+    return std::visit(PrintStmt{}, stmtNode);
 }
 
 std::string PrintExpr::operator()(const LiteralExprPtr& expr) const {
@@ -25,8 +33,14 @@ std::string PrintExpr::operator()(const BinaryExprPtr& expr) const {
     return parenthesize(expr->m_operation.lexeme, {&expr->m_leftNode, &expr->m_rightNode});
 }
 
-std::string parenthesize(std::string_view name,
-                         const std::initializer_list<const ExprNodePtr*> exprNodes) {
+std::string PrintExpr::operator()(const IdentifierExprPtr& expr) const {
+    return expr->m_Name.lexeme;
+}
+
+// (name expr...)
+std::string
+PrintExpr::parenthesize(const std::string& name,
+                        const std::initializer_list<const ExprNodePtr*> exprNodes) const {
 
     std::stringstream s;
     s << '(' << name;
@@ -34,6 +48,53 @@ std::string parenthesize(std::string_view name,
         s << " ";
         s << std::visit(PrintExpr{}, *expr);
     }
-    s << " )";
+    s << ')';
     return s.str();
+}
+
+std::string PrintStmt::operator()(const ExprStmtPtr& stmt) const {
+    return Printer::printExpr(stmt->m_Expr);
+}
+
+std::string PrintStmt::operator()(const ReturnStmtPtr& stmt) const {
+    return std::format("RETURN: {}", Printer::printExpr(stmt->m_RetValue));
+}
+
+std::string PrintStmt::operator()(const IfStmtPtr& stmt) const {
+    return std::format("IF\n\tCONDITION:{}\n{}", Printer::printExpr(stmt->m_Condition),
+                       indent("BODY:", {&stmt->m_Body}));
+}
+
+std::string PrintStmt::operator()(const FunctionStmtPtr& stmt) const {
+    return std::format("FUNCTION \"{}\"\n  {}", stmt->m_Identifier.lexeme,
+                       indent("BODY", {&stmt->m_Body}));
+}
+
+std::string PrintStmt::indent(const std::string& name,
+                              std::initializer_list<const StmtNodePtr*> stmtNodes) const {
+    std::stringstream s;
+    s << name;
+    for (const auto* stmt : stmtNodes) {
+        s << "\n  | ";
+        s << std::visit(PrintStmt{}, *stmt);
+    }
+    s << '\n';
+    return s.str();
+}
+
+int main() {
+    auto Lnode = createAstNode<LiteralExpr>(4);
+    auto L2 = createAstNode<LiteralExpr>(1);
+    auto Rnode = createAstNode<LiteralExpr>(1);
+
+    auto plus = Token(TokenType::PLUS, "+", 1, 1);
+    auto star = Token(TokenType::STAR, "*", 1, 1);
+
+    auto B = createAstNode<BinaryExpr>(std::move(Lnode), plus, std::move(Rnode));
+    auto A = createAstNode<BinaryExpr>(std::move(L2), star, std::move(B));
+    auto rt = createAstNode<ReturnStmt>(std::move(A));
+
+    Token name = Token(TokenType::IDENTIFIER, "main", 1, 2);
+    auto func = createAstNode<FunctionStmt>(std::move(name), std::move(rt));
+    std::println("{}", Printer::printStmt(func));
 }
--- src/parser/AstPrinter.hpp
+++ src/parser/AstPrinter.hpp
@@ -1,15 +1,33 @@
-
+#pragma once
 #include "Ast/Expr.hpp"
+#include "Ast/Stmt.hpp"
 
+class Printer {
+
+  public:
+    static std::string printExpr(const ExprNodePtr& exprNode);
+    static std::string printStmt(const StmtNodePtr& stmtNode);
+};
+
+// PRINTING EXPRESSIONS
 struct PrintExpr {
     std::string operator()(const LiteralExprPtr& expr) const;
-
     std::string operator()(const BinaryExprPtr& expr) const;
-
     std::string operator()(const IdentifierExprPtr& expr) const;
 
-    std::string parenthesize(std::string_view name,
+    std::string parenthesize(const std::string& name,
                              std::initializer_list<const ExprNodePtr*> exprNodes) const;
 };
 
-std::string Print(ExprNodePtr exprNode);
+// PRINTING STATEMENTS
+struct PrintStmt {
+    std::string operator()(const ExprStmtPtr& stmt) const;
+    std::string operator()(const ReturnStmtPtr& stmt) const;
+    std::string operator()(const IfStmtPtr& stmt) const;
+    std::string operator()(const FunctionStmtPtr& stmt) const;
+
+    std::string indent(const std::string& name,
+                       std::initializer_list<const StmtNodePtr*> stmtNodes) const;
+};
+
+void PrintAST(const ExprNodePtr& exprNode);
