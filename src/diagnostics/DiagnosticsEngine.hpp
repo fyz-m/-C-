487ff3cb7fb050f20754001b20e1d36d014500f5
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-04 15:22:35 +0400

    Made members inline

--- src/diagnostics/DiagnosticsEngine.hpp
+++ src/diagnostics/DiagnosticsEngine.hpp
@@ -4,26 +4,37 @@
 #include <string_view>
 #include <vector>
 
-namespace Diagostics {
-
-struct LineInformation;
-struct Diagnostic;
+namespace Diagnostics {
 
 enum class ErrorType { LexerError, SyntaxError };
 enum class Severity { Fatal, Warning };
 
+// Stores where each line starts/ends as indices into the source string buffer
+struct LineInformation {
+    size_t start{};
+    size_t end{};
+};
+
+struct Diagnostic {
+    Token* token{};
+    std::string_view errorMessage{};
+    ErrorType type{};
+};
+
 class DiagnosticsEngine {
   private:
     // String buffer holding the source code file contents
-    static std::string_view m_source;
+    inline static std::string_view m_source;
     // Keep track of each line so entire line can be printed in error
-    static std::vector<LineInformation> m_lineInfo;
-    static size_t m_errorCount;
+    inline static std::vector<LineInformation> m_lineInfo;
+    inline static size_t m_errorCount;
 
   public:
     static void init(std::string_view m_source);
     static void report(Token& token, std::string_view errorMessage, ErrorType type);
     static void addLine(size_t lineStart, size_t lineEnd);
+
+    static void printLn(size_t line);
 
     DiagnosticsEngine() = delete;
 
@@ -32,16 +43,4 @@ class DiagnosticsEngine {
     void renderDiagnostic(Diagnostic& diagnostic);
 };
 
-// Stores where each line starts/ends as indices into the source string buffer
-struct LineInformation {
-    size_t start;
-    size_t end;
-};
-
-struct Diagnostic {
-    Token* token;
-    std::string_view errorMessage;
-    ErrorType type;
-};
-
-} // namespace Diagostics
+} // namespace Diagnostics
