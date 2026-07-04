c5ced17aceab6b760a794d8cd38660f4d349a028
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-04 13:17:01 +0400

    Fix dangling reference being lexed

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "lexer/lexer.hpp"
+#include "parser/AstPrinter.hpp"
+#include "parser/parser.hpp"
 
 #include <fstream>
 #include <print>
@@ -8,12 +10,16 @@ void run(const std::string& filepath);
 
 int main(int argc, char* argv[]) {
 
-    auto* filepath = argv[1];
-    run(filepath);
+    if (argc != 2) {
+        std::println("Usage: ./main program.c");
+        return 1;
+    }
+    run(argv[1]);
 }
 
 void run(const std::string& filepath) {
-    std::ifstream file(filepath);
+
+    std::ifstream file(filepath, std::ios::binary | std::ios::in);
     if (file.fail()) {
         std::println("Could not open file '{}'.", filepath);
         return;
@@ -21,7 +27,16 @@ void run(const std::string& filepath) {
 
     std::stringstream text;
     text << file.rdbuf();
+    auto source = text.str();
+    Lexer lexer(source);
+    auto tokens = lexer.Tokenize();
 
-    Lexer lexer;
-    lexer.Tokenize(text.str());
+    if (!tokens.has_value()) {
+        std::println("Lexer error");
+        return;
+    }
+
+    Parser parser = Parser(tokens.value());
+    auto AST = parser.Parse();
+    AST::Printer::printAST(AST);
 }
