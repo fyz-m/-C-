a171b6b1986dbfadc764d50897dff86924c7bbf3
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-12 18:29:38 +0400

    Added main helper functions

--- src/IR/IRgen.cpp
+++ src/IR/IRgen.cpp
@@ -8,16 +8,38 @@ stored as a "quadruple"
  - destination
  - src1
  - src2
+
+ We need functions that emit commonly used operations
+ such as loading a literal
 */
 
 namespace IR {
 
-size_t Generator::nextRegister() {
-    return m_CurrRegister++;
+size_t Generator::emitExpr(const ExprNodePtr& expr) {
+    return std::visit(EmitExpr{*this}, expr);
 }
 
-void Generator::emit(OPCODE op, size_t src1, size_t src2, size_t dest) {
-    m_QuadList.emplace_back(op, src1, src2, dest);
+size_t Generator::emitStmt(const StmtNodePtr& stmt) {
+    return std::visit(EmitStmt{*this}, stmt);
+}
+
+// dst = LI imm
+size_t Generator::loadIntToReg(int integer) {
+    return emit(nextRegister(), OPCODE::LI, integer);
+}
+
+size_t Generator::emit(size_t dest, OPCODE op, std::optional<size_t> src1,
+                       std::optional<size_t> src2) {
+    m_QuadList.emplace_back(dest, op, src1, src2);
+    return dest;
+}
+
+void Generator::emit(OPCODE op, std::optional<size_t> src1, std::optional<size_t> src2) {
+    m_QuadList.emplace_back(std::nullopt, op, src1, src2);
+}
+
+size_t Generator::nextRegister() {
+    return m_CurrRegister++;
 }
 
 } // namespace IR
--- src/IR/IRgen.hpp
+++ src/IR/IRgen.hpp
@@ -1,33 +1,49 @@
 #pragma once
-#include "parser/Ast/Expr.hpp"
+#include "emit.hpp"
 
-#include <cstdint>
 #include <optional>
 #include <vector>
 
 namespace IR {
-struct ConvertExpr;
 
-enum class OPCODE : std::uint8_t { ADD, SUB, DIV, MULT, LI, NEG };
+enum class OPCODE : std::uint8_t { ADD, SUB, DIV, MULT, LI, NEG, RET };
 
 struct Quadruple {
     std::optional<size_t> Src1; // source operands
     std::optional<size_t> Src2;
     OPCODE Op;
-    size_t Dst; // destination of the result of: Src1 Op Src2
+    std::optional<size_t> Dst; // destination of the result of: Src1 Op Src2
 
-    Quadruple(size_t dst, OPCODE op, std::optional<size_t> src1, std::optional<size_t> src2)
+    Quadruple(std::optional<size_t> dst, OPCODE op, std::optional<size_t> src1,
+              std::optional<size_t> src2)
         : Src1{src1}, Src2{src2}, Op{op}, Dst{dst} {}
+
+    // For testing
+    bool operator==(const Quadruple& other) const {
+        if (this->Op != other.Op)
+            return false;
+        if (this->Src1 != other.Src1)
+            return false;
+        if (this->Src2 != other.Src2)
+            return false;
+        if (this->Dst != other.Dst)
+            return false;
+
+        return true;
+    }
 };
 
 class Generator {
-    friend struct ConvertExpr;
+    friend struct EmitExpr;
+    friend struct EmitStmt;
 
   private:
     std::vector<Quadruple> m_QuadList;
     size_t m_CurrRegister;
 
   public:
+    size_t emitStmt(const StmtNodePtr& stmt);
+
     // emit an expression, returning the register that contains the value of the expression
     // evaluated
     size_t emitExpr(const ExprNodePtr& expr);
@@ -38,28 +54,43 @@ class Generator {
     size_t loadIntToReg(int integer);
 
   private:
-    // Push a quadruple onto the list, returns dst for convienience
+    // Push a quadruple onto the list, returns dst (last used register)
     size_t emit(size_t dest, OPCODE op, std::optional<size_t> src1 = std::nullopt,
                 std::optional<size_t> src2 = std::nullopt);
+
+    void emit(OPCODE op, std::optional<size_t> src1 = std::nullopt,
+              std::optional<size_t> src2 = std::nullopt);
+
+    size_t nextRegister();
 
     // Convert operator into its corresponding opcode in the TAC IR
     // different method for unary because same operator can translate to different operation
     // E.g '-' in binary is subtraction wheras is it negation in unary
-    constexpr OPCODE getOpcodeBinary(TokenType _operator);
+    constexpr OPCODE getOpcodeBinary(TokenType _operator) {
+        switch (_operator) {
+        case TokenType::PLUS:
+            return OPCODE::ADD;
+        case TokenType::MINUS:
+            return OPCODE::SUB;
+        case TokenType::STAR:
+            return OPCODE::MULT;
+        case TokenType::SLASH:
+            return OPCODE::DIV;
 
-    constexpr OPCODE getOpcodeUnary(TokenType _operator);
+        default:
+            // unreachable
+        }
+    }
 
-    size_t nextRegister();
+    constexpr OPCODE getOpcodeUnary(TokenType _operator) {
+        switch (_operator) {
+        case TokenType::MINUS:
+            return OPCODE::NEG;
+
+        default:
+            // unreachable
+        }
+    }
 };
 
-struct ConvertExpr {
-    Generator& gen;
-    ConvertExpr(Generator& gen)
-        : gen{gen} {}
-    size_t operator()(const LiteralExprPtr& expr) const;
-    size_t operator()(const BinaryExprPtr& expr) const;
-    size_t operator()(const UnaryExprPtr& expr) const;
-    size_t operator()(const IdentifierExprPtr& expr) const;
-    size_t operator()(const AssignmentExprPtr& expr) const;
-};
 } // namespace IR
