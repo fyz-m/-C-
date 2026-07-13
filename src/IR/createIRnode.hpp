159dd511b42b071a693def2b02f0efd0510aa5d0
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-13 19:17:20 +0400

    Renamed concept to prevent naming collision with IRnode variant

--- src/IR/createIRnode.hpp
+++ src/IR/createIRnode.hpp
@@ -3,9 +3,9 @@
 #include "IRnodes.hpp"
 
 template <typename T>
-concept IRnode = std::same_as<T, IR::BinaryNode> || std::same_as<T, IR::UnaryNode>;
+concept IRnodeType = std::same_as<T, IR::BinaryNode> || std::same_as<T, IR::UnaryNode>;
 
-template <IRnode T, typename... Args>
+template <IRnodeType T, typename... Args>
 auto createIRnode(Args... args) {
     auto ptr = std::make_unique<T>(std::forward<Args>(args)...);
     return IR::IRnode{std::move(ptr)};
