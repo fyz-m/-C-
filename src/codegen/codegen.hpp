1b3b36ce6ecef4e0cff775d9fb40321326bb6145
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-16 15:21:13 +0400

    Created helper to down cast an IR operand to a RISC-V operand

--- src/codegen/codegen.hpp
+++ src/codegen/codegen.hpp
@@ -7,6 +7,11 @@
 
 namespace CODEGEN {
 
+template <class... Ts>
+struct Overloaded : Ts... {
+    using Ts::operator()...;
+};
+
 // The code generator maps each TAC IR construct to a construct in a
 // lower level IR that resembles RISC-V assembly. It determines the
 // the required RISC-V instructions that are equivalent to the IR
@@ -35,6 +40,42 @@ class CodeGenerator {
     std::vector<RISCV::Instruction>& generateRISCVassembly();
 
   private:
+    // Convert an IR operand into a RISC-V operand. Because a
+    // RISC-V operand cannot be an integer (immediates are stored as
+    // int, not in the operand variant).
+    // An IR operand that is an integer must be handled in the
+    // IRvisitor before casting dowm the variant.
+    static RISCV::Operand castVariant(IR::Operand& operand) {
+
+        auto op = RISCV::Operand{};
+
+        std::visit(
+            Overloaded{
+                [](int) {
+                    // Should be unreachable as
+                    // function should never be called
+                    // if operand contains <int> (enforced in
+                    // IRvisitor control flow)
+                    throw std::runtime_error(
+                        "Attempt to convert integer into "
+                        "RISC-V operand");
+                },
+
+                [&](IR::VirtualRegister) {
+                    op = RISCV::Operand{
+                        std::get<IR::VirtualRegister>(operand)};
+                },
+
+                [&](std::string&) {
+                    op = RISCV::Operand{
+                        std::get<std::string>(operand)};
+                },
+            },
+            operand);
+
+        return op;
+    }
+
     // Wrapper that pushes the instruction onto the vector
     template <RISCV::RV_INSTR T, typename... Args>
     void pushInstruction(Args... args) {
