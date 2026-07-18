502a0919ff3ed5d015a2bd90d5b6b14853ed547c
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-18 22:29:30 +0400

    Changed logic so that binary and unary node visitors store their result conditionally in either a user-defined or compiler generated variable depending on if an assignment expression is being translated.

--- src/IR/IRgen.cpp
+++ src/IR/IRgen.cpp
@@ -19,7 +19,8 @@ void StmtVisitor::operator()(const ExprStmtPtr& stmt) const {
 void StmtVisitor::operator()(const BlockStmtPtr& stmt) const {}
 
 void StmtVisitor::operator()(const ReturnStmtPtr& stmt) const {
-    auto retval = std::visit(ExprVisitor{this->Gen}, stmt->m_RetValue.value());
+    auto retval =
+        std::visit(ExprVisitor{this->Gen}, stmt->m_RetValue.value());
     Gen.emit<IR::ReturnNode>(std::move(retval));
 }
 
@@ -27,7 +28,8 @@ void StmtVisitor::operator()(const IfStmtPtr& stmt) const {}
 
 void StmtVisitor::operator()(const FunctionStmtPtr& stmt) const {}
 
-void StmtVisitor::operator()(const VarDeclarationStmtPtr& stmt) const {}
+void StmtVisitor::operator()(
+    const VarDeclarationStmtPtr& stmt) const {}
 
 Operand ExprVisitor::operator()(const LiteralExprPtr& expr) const {
     if (auto* p = std::get_if<int>(&expr->m_Value))
@@ -38,46 +40,77 @@ Operand ExprVisitor::operator()(const LiteralExprPtr& expr) const {
 
 Operand ExprVisitor::operator()(const BinaryExprPtr& expr) const {
 
-    auto src1 = std::visit(*this, expr->m_leftNode);
-    auto src2 = std::visit(*this, expr->m_rightNode);
-    auto result = Gen.getTempVar();
+    auto src1 = std::visit(ExprVisitor{Gen}, expr->m_leftNode);
+    auto src2 = std::visit(ExprVisitor{Gen}, expr->m_rightNode);
 
-    Gen.emit<IR::BinaryNode>(result,
-                             std::move(src1),
-                             Generator::getBinaryOp(expr->m_operation.type),
-                             std::move(src2));
+    // if caller was an AssignmentExpr visitor, the result of this
+    // expression is stored in the variable that the expression is
+    // being assigned to.
+    Variable result;
+    if ((*this).Resultname.has_value())
+        result = Resultname.value();
+    // else create new variable and store result in that
+    else
+        result = Gen.getTempVar();
+
+    Gen.emit<IR::BinaryNode>(
+        result,
+        std::move(src1),
+        Generator::getBinaryOp(expr->m_operation.type),
+        std::move(src2));
+
     return result;
 }
 
 Operand ExprVisitor::operator()(const UnaryExprPtr& expr) const {
 
-    auto src1 = std::visit(*this, expr->m_expression);
-    auto result = Gen.getTempVar();
+    auto src1 = std::visit(ExprVisitor{Gen}, expr->m_expression);
+
+    Variable result;
+
+    if ((*this).Resultname.has_value())
+        // Need to cast the std::optional for move semantics
+        result = std::move(Resultname).value();
+    else
+        result = Gen.getTempVar();
 
     Gen.emit<IR::UnaryNode>(
-        result, Generator::getUnaryOp(expr->m_operation.type), std::move(src1));
+        result,
+        Generator::getUnaryOp(expr->m_operation.type),
+        std::move(src1));
     return result;
 }
 
 Operand ExprVisitor::operator()(const IdentifierExprPtr& expr) const {
-    auto result = Gen.getTempVar();
-    Gen.emit<IR::AssignToVreg>(result, expr->m_Name.lexeme);
-    return result;
+    return expr->m_Name.lexeme;
 }
 
 Operand ExprVisitor::operator()(const AssignmentExprPtr& expr) const {
-    auto src1 = std::visit(*this, expr->m_Value);
-    std::string name = expr->m_Identifier->m_Name.lexeme;
 
-    Gen.emit<IR::AssignmentNode>(expr->m_Identifier->m_Name.lexeme,
+    std::string var_name =
+        std::move(expr->m_Identifier->m_Name.lexeme);
+
+    auto src1 = std::visit(ExprVisitor{Gen, var_name}, expr->m_Value);
+
+    // If the exprNode (Value) that the AssignmentNode held did not
+    // nest deeper (i.e contained another exprNode), the result of
+    // converting that expr into 3AC (src1) would land itself to only
+    // one 3AC instruction.
+    // Meaning src1 == var_name,
+    auto* res = std::get_if<std::string>(&src1);
+    if (res && *res == var_name)
+        return var_name;
+
+    auto var_name_cpy = var_name;
+    Gen.emit<IR::AssignmentNode>(std::move(var_name),
                                  std::move(src1));
-    return name;
+    return var_name_cpy;
 }
 
 Variable Generator::getTempVar(bool floating_point) {
     // '.' is not allowed as an identifier
-    // We use it for compiler generated variables so there is no collision with
-    // user-defined variables
+    // We use it for compiler generated variables so there is no
+    // collision with user-defined variables
     if (floating_point)
         return std::format("tfp.{}", m_CurrRegisterFP++);
 
