31d2f491440b8ad61e06c2001c6f2877d2f2b3a1
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-18 22:31:08 +0400

    Added an internal state to ExprVisitor to allow AssignmentExpr visitor to change the result name of an instruction

--- src/IR/IRgen.hpp
+++ src/IR/IRgen.hpp
@@ -7,37 +7,10 @@
 #include <vector>
 
 namespace IR {
-
-class Generator;
-
-// Visitors for AST nodes
-// Translates a given AST node into a 3AC IR node(s)
-struct ExprVisitor {
-    Generator& Gen;
-    ExprVisitor(Generator& generator)
-        : Gen{generator} {}
-
-    Operand operator()(const LiteralExprPtr& expr) const;
-    Operand operator()(const BinaryExprPtr& expr) const;
-    Operand operator()(const UnaryExprPtr& expr) const;
-    Operand operator()(const IdentifierExprPtr& expr) const;
-    Operand operator()(const AssignmentExprPtr& expr) const;
-};
-
-struct StmtVisitor {
-    Generator& Gen;
-    StmtVisitor(Generator& generator)
-        : Gen{generator} {}
-
-    void operator()(const ExprStmtPtr& stmt) const;
-    void operator()(const BlockStmtPtr& stmt) const;
-    void operator()(const ReturnStmtPtr& stmt) const;
-    void operator()(const IfStmtPtr& stmt) const;
-    void operator()(const FunctionStmtPtr& stmt) const;
-    void operator()(const VarDeclarationStmtPtr& stmt) const;
-};
-
+struct ExprVisitor;
 class Generator {
+
+    friend struct ExprVisitor;
 
   private:
     std::vector<IRnode> m_Nodes;
@@ -56,16 +29,17 @@ class Generator {
     // the Nodes vector
     template <IRnodeType T, typename... Args>
     void emit(Args... args) {
-        m_Nodes.push_back(createIRnode<T>(std::forward<Args>(args)...));
+        m_Nodes.push_back(
+            createIRnode<T>(std::forward<Args>(args)...));
     }
 
     // Load an integer literal into a register
     Variable loadVarToReg(int integer);
 
     // Create a new variable
-    // We need to make new variables so we can store the intermediate result(s)
-    // of expressions with more than 2 operands, because 3AC IR only allows a
-    // maximum of two operands
+    // We need to make new variables so we can store the intermediate
+    // result(s) of expressions with more than 2 operands, because 3AC
+    // IR only allows a maximum of two operands
     Variable getTempVar(bool floating_point = false);
 
     // Convert operator into its corresponding OPERATION in the TAC IR
@@ -102,4 +76,50 @@ class Generator {
     }
 };
 
+// Visitors for AST nodes
+// Translates a given AST node into a 3AC IR node(s)
+struct ExprVisitor {
+
+    Generator& Gen;
+
+    // This is used by AssignmentExprPtr visitor to tell the callee
+    // that its result should be stored in this name. Otherwise store
+    // result in a new variable.
+    // e.g Assignment:
+    // a = 1 + 2 -> Resultname = "a" -> BinaryNode(a, 1, PLUS, 2)
+    // Expr:
+    // 1 + 2 -> BinaryNode(t.0, 1, PLUS, 2);
+    std::optional<Variable> Resultname;
+
+    ExprVisitor(Generator& generator)
+        : Gen{generator} {}
+
+    ExprVisitor(Generator& generator, Variable var)
+        : Gen{generator}, Resultname{std::move(var)} {}
+
+    Operand operator()(const LiteralExprPtr& expr) const;
+
+    Operand operator()(const BinaryExprPtr& expr) const;
+
+    Operand operator()(const UnaryExprPtr& expr) const;
+
+    Operand operator()(const IdentifierExprPtr& expr) const;
+
+    Operand operator()(const AssignmentExprPtr& expr) const;
+};
+
+struct StmtVisitor {
+
+    Generator& Gen;
+
+    StmtVisitor(Generator& generator)
+        : Gen{generator} {}
+
+    void operator()(const ExprStmtPtr& stmt) const;
+    void operator()(const BlockStmtPtr& stmt) const;
+    void operator()(const ReturnStmtPtr& stmt) const;
+    void operator()(const IfStmtPtr& stmt) const;
+    void operator()(const FunctionStmtPtr& stmt) const;
+    void operator()(const VarDeclarationStmtPtr& stmt) const;
+};
 } // namespace IR
