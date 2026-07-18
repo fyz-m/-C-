4afcff7695a004a9306f180ba08b7d8a35a21064
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-18 22:31:31 +0400

    Updated tests to reflect changes in IR

--- tests/test_IR.cpp
+++ tests/test_IR.cpp
@@ -26,7 +26,6 @@ TEST_P(GenIRtest, expression) {
 
     IR::Generator generator = IR::Generator(nodes.value());
     auto output = IR::Printer::printIR(generator.generateIR());
-
     EXPECT_EQ(output, tc.expected);
 }
 
@@ -36,53 +35,57 @@ INSTANTIATE_TEST_SUITE_P(
 
     ::testing::Values
     (
-        IRTestCase("binary", "1 + 2;", "t0 = 1 + 2\n"),
-        IRTestCase("binary_var", "a + b;", "t0 = a + b\n"),
+        IRTestCase("binary", "1 + 2;", "t.0 = 1 + 2\n"),
+        IRTestCase("binary_var", "a + b;", "t.0 = a + b\n"),
         IRTestCase("binary_multiple_operands", "1 + 2 + 3 + 4;", 
-                    "t0 = 1 + 2\n" 
-                    "t1 = t0 + 3\n"
-                    "t2 = t1 + 4\n"
+                    "t.0 = 1 + 2\n" 
+                    "t.1 = t.0 + 3\n"
+                    "t.2 = t.1 + 4\n"
                  ),
-        IRTestCase("unary", "-2;", "t0 = ~ 2\n"),
-        IRTestCase("unary_var", "-2;", "t0 = ~ 2\n"),
+        IRTestCase("unary_complement", "~2;", "t.0 = ~ 2\n"),
+        IRTestCase("unary_negation", "-2;", "t.0 = - 2\n"),
+        IRTestCase("unary_var", "-2;", "t.0 = - 2\n"),
+
+        IRTestCase("unary_chained", "-~2;", 
+            "t.0 = ~ 2\n" 
+            "t.1 = - t.0\n"),
 
         IRTestCase("precedence", "1 * -2 / 3 + 4;", 
-                    "t0 = ~ 2\n" 
-                    "t1 = 1 * t0\n"
-                    "t2 = t1 / 3\n"
-                    "t3 = t2 + 4\n"
+                    "t.0 = - 2\n" 
+                    "t.1 = 1 * t.0\n"
+                    "t.2 = t.1 / 3\n"
+                    "t.3 = t.2 + 4\n"
                  ),
         IRTestCase("precedence_grouping", "1 * -x / (y + -4);", 
-                    "t0 = ~ x\n" 
-                    "t1 = 1 * t0\n"
-                    "t2 = ~ 4\n"
-                    "t3 = y + t2\n"
-                    "t4 = t1 / t3\n"
+                    "t.0 = - x\n" 
+                    "t.1 = 1 * t.0\n"
+                    "t.2 = - 4\n"
+                    "t.3 = y + t.2\n"
+                    "t.4 = t.1 / t.3\n"
                  ),
         IRTestCase("assignment", "x = 4;", "x = 4\n"),
         IRTestCase("assignment_2", "x = 4; y = 3 * 4; x = y;", 
                     "x = 4\n"
-                    "t0 = 3 * 4\n"
-                    "y = t0\n"
+                    "y = 3 * 4\n"
                     "x = y\n"
                 ),
         IRTestCase("chained_assignment", "a = 3; a = b = c = 3;", 
-                    "x = 4\n"
-                    "t0 = 3 * 4\n"
-                    "y = t0\n"
-                    "x = y\n"
+                    "a = 3\n"
+                    "c = 3\n"
+                    "b = c\n"
+                    "a = b\n"
                 ),
 
         IRTestCase("return", "return 7;", "ret 7\n"),
         IRTestCase("return_var", "return a;", "ret a\n"),
         IRTestCase("return_expr", "return  -4 * 2;", 
-                    "t0 = ~ 4\n"
-                    "t1 = t0 * 2\n"
-                    "ret t1\n"
+                    "t.0 = - 4\n"
+                    "t.1 = t.0 * 2\n"
+                    "ret t.1\n"
                 ),
 
 
-        IRTestCase("addition9", "1 + 2;", "t0 = 1 + 2\n")
+        IRTestCase("addition9", "1 + 2;", "t.0 = 1 + 2\n")
     ),
 
     [](const ::testing::TestParamInfo<IRTestCase>& info) {
