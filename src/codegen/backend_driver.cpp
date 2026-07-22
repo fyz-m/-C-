ef8f503b8ec698f537cf42daa438ea2088b5461f
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-07-22 17:15:18 +0400

    Added api for converting pseudo instructions

--- src/codegen/backend_driver.cpp
+++ src/codegen/backend_driver.cpp
@@ -1,14 +1,19 @@
 #include "code_emission.hpp"
 #include "codegen.hpp"
+#include "expand_pseudo/expand_pseudo.hpp"
 #include "reg_alloc/register_allocator.hpp"
 
 std::string CODEGEN::generateASM(std::span<IR::IRnode> nodes,
-                                 bool abi_regs) {
+                                 bool abi_regs,
+                                 bool convertPseudo) {
     auto generator = CODEGEN::CodeGenerator{nodes};
     generator.generateRISCVassembly();
 
     auto& instructions = generator.getInstructions();
     // CODEGEN::RegisterAllocator().varToMem(instructions);
+
+    if (convertPseudo)
+        convertPseudoInstructions(instructions);
 
     auto emitter = CODEGEN::CodeEmitter{abi_regs};
     return emitter.emitAsm(instructions);
